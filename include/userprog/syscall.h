846d0556100e3aaac35e39f9b980c41688ced78c
enaa99 <wprkfdlsgk@gmail.com>
2022-12-27 20:53:21 +0900

    syscall handler

--- include/userprog/syscall.h
+++ include/userprog/syscall.h
@@ -4,7 +4,4 @@
 void syscall_init (void);
 
 
-void check_address(void *addr);
-
-
 #endif /* userprog/syscall.h */
--- userprog/syscall.c
+++ userprog/syscall.c
@@ -10,6 +10,13 @@
 
 void syscall_entry (void);
 void syscall_handler (struct intr_frame *);
+void check_address(void *addr);
+
+
+bool create(const char *file, unsigned initial_size);
+bool remove(const char *file);
+void halt(void);
+void exit(int status);
 
 /* System call.
  *
@@ -36,46 +43,89 @@ syscall_init (void) {
 	write_msr(MSR_SYSCALL_MASK,
 			FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
 }
-
-/* The main system call interface */
 void
-syscall_handler (struct intr_frame *f UNUSED) {
-	// TODO: Your implementation goes here.
-	check_address(f->rsp);
-
-	switch(f->R.rax) {
-		case SYS_HALT:
-			halt();
-			break;
-		case SYS_EXIT:
-			exit(f->R.rdi);
-			break;
-		case SYS_CREATE:
-			create(f->R.rdi, f->R.rsi);
-			break;
-		case SYS_REMOVE:
-			remove(f->R.rdi);
-			break;
-		case SYS_WRITE:
-			f->R.rax = write(f->R.rdi, f->R.rsi, f->R.rdx);
-			break;
-		case SYS_OPEN:
-			f->R.rax = open(f->R.rdi);
-			break;
-		default:
-			exit(-1);
-			break;
+check_address (void *addr) {
+	struct thread *current_thread = thread_current();
+	
+	if (is_kernel_vaddr(addr) || pml4_get_page(current_thread->pml4, addr) == NULL) {
+		exit(-1);
 	}
-
-
-	// printf ("system call!\n");
-	// thread_exit ();
 }
 
-void check_address(void *addr)
+void syscall_handler(struct intr_frame *f UNUSED)
 {
-	struct thread* cur = thread_current();
-	// 커널주소인지 - 유저공간에서 할당되었는지
-	if(is_kernel_vaddr(addr) || pml4_get_page(cur->pml4,addr) == NULL)
-		exit(-1);
+    switch (f->R.rax) 
+    {
+    case SYS_HALT:
+        halt();
+        break;
+    case SYS_EXIT:
+        exit(f->R.rdi); 
+        break;
+    case SYS_FORK:
+		// fork();
+		break;
+    case SYS_EXEC:
+		// exec();
+        break;
+    case SYS_WAIT:
+        // process_wait();
+        break;
+    case SYS_CREATE:
+        create(f->R.rdi,f->R.rsi);
+        break;
+    case SYS_REMOVE:
+        remove(f->R.rdi);
+        break;
+    case SYS_OPEN:
+        // open();
+        break;
+    case SYS_FILESIZE:
+        // filesize();
+        break;
+    case SYS_READ:
+        // read();
+        break;
+    case SYS_WRITE:
+        // write();
+        break;
+    case SYS_SEEK:
+        // seek();
+        break;
+    case SYS_TELL:
+        // tell();
+        break;
+    case SYS_CLOSE:
+        // close();
+        break;
+    default:
+        exit(-1);
+        break;
+    }
+}
+
+void halt(void){
+	power_off();
+}
+
+void exit(int status)
+{
+	struct thread *cur = thread_current();
+	cur->status = status;
+	
+	printf("%s : exit(%d)",cur->name,status);
+
+	thread_exit();
+}
+
+bool create(const char *file, unsigned initial_size)
+{
+	check_address(file);
+	return filesys_create(file,initial_size);
+}
+
+bool remove(const char *file)
+{
+	check_address(file);
+	return filesys_remove(file);
 }
